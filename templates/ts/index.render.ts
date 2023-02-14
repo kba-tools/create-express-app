4c54d4e58b2629f38d8233943e9695d74a1132ef
440181N-440H4N4N <mobinm.mohanan@gmail.com>
2023-02-14 17:02:13 +0530

    view_components

---
+++ templates/ts/index.render.ts
@@ -0,0 +1,9 @@
+import { Request, Response, Router } from "express";
+const router = Router();
+
+/* GET home page. */
+router.get("/", function (req: Request, res: Response) {
+  res.render("index", { title: "Express" });
+});
+
+export default router;
